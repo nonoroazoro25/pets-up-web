53b25e4ee66e528a8d715e7434c9fc9a8192acef
SZ\yansha <yansha@rayvision.com>
2022-12-27 17:46:27 +0800

    menu bar

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,108 +1,55 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="avatar_box">
-            </div>
-            <!-- 表单区域 -->
-            <div class="login_form">
-              <p class="title">登录</p>
-              <el-form :model="dataForm" :rules="dataRule" @keyup.enter.native="dataFormSubmit()">
-                  <!-- 用户名 -->
-                  <el-form-item prop="userName">
-                      <el-input v-model="dataForm.userName" ref="dataForm" prefix-icon="el-icon-user-solid"></el-input>
-                  </el-form-item>
-                  <!-- 密码 -->
-                  <el-form-item prop="password">
-                      <el-input prefix-icon="el-icon-lock" v-model="dataForm.password" type="password"></el-input>
-                  </el-form-item>
-                  <!-- 按钮 -->
-                  <el-form-item class="btns">
-                      <el-button type="primary" @click="PetsLogin()">ddd</el-button>
-                      <el-button type="info">重置</el-button>
-                      <p @click="getRegisterPage">没有账号？立即注册</p>
-                  </el-form-item>
-              </el-form>
-            </div>
-        </div>
-    </div>
-  </template>
+  <el-row class="tac">
+  <el-col :span="3">
+    <h5>Pet's UP</h5>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>最近记录</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">饮食</template>
+          <el-menu-item index="1-1">早</el-menu-item>
+          <el-menu-item index="1-2">中</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="要事记录">
+          <el-menu-item index="1-3">疫苗</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">宠物列表</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">设置</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+</el-row>
+</template>
   
-  <script>
-  export default {
-    data () {
-      return {
-        dataForm: {
-          userName: '',
-          password: ''
-        },
-        dataRule: {
-          userName: [
-            { required: true, message: '帐号不能为空', trigger: 'blur' }
-          ],
-          password: [
-            { required: true, message: '密码不能为空', trigger: 'blur' }
-          ]
-        }
-      }
-    },
- 
+<script>
+    export default {
     methods: {
-      
-      // 提交表单
-      dataFormSubmit () {
-        alert('submit!');
-
-      },
-
-      getRegisterPage (){
-        this.$router.push({
-          path: '/register',
-        })
-      },
-
-      PetsLogin () {
-        this.$axios.post('http://localhost:2020/api/login', {
-          username: 'ys',
-          password: 'qwer.1234'
-        }).then(({data}) => {
-          if (data && data.code === 0) {
-            console.log('login success');
-            this.$router.push({ name: 'home' })
-          }
-        })
-      }
-
-    },
-
-    
-
+        handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+        console.log(key, keyPath);
+        }
     }
-  
+    }
 </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  .login_form {
-    position: absolute;
-    bottom: 200px;
-    width:30%;
-    padding:20px 20px;
-    box-sizing: border-box;
-  }
+<style scoped>
 
-  .btns {
-    display: flex;
-    justify-content:flex-end;
-  }
 
-  .title {
-    font-size: 26px;
-    line-height: 50px;
-    font-weight: bold;
-    margin: 10px;
-    text-align: center;
-  }
-
-  </style>
+</style>
   
