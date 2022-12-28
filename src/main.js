e0bcb556cf756d51e9fbc2087c3b378ec41327d3
nonoroazoro25 <814472228@qq.com>
2022-12-28 18:32:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import axios from 'axios';
-import store from './store'
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
@@ -16,7 +15,6 @@ Vue.prototype.$axios = axios;
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,29 @@
-/**
- * 全站路由配置
- *
- * 建议:
- * 1. 代码中路由统一使用name属性跳转(不使用path属性)
- */
 import Vue from 'vue'
 import Router from 'vue-router'
-import main from '../views/main'
-import home from '../views/common/home'
-import login from '../views/common/login'
-
+import Login from '@/views/common/Login'
+import register from '@/views/common/register'
+import home from '@/views/common/home'
 
 Vue.use(Router)
 
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'main',
-      component: main,
-      redirect: { name: 'home' },
-      children: [
-        { path: '/home', component: home, name: 'home', meta: { title: '首页' } },
-      ]
 
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: register
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: home
     }
   ]
 })
